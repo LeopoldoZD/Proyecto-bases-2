5e5473faf948461ff18224c783f169349a7e674c
rapid <rapid@10.20.0.78>
2016-09-28 20:10:13 -0500

    hoy 28 de sep

--- src/java/modelo/Conexion.java
+++ src/java/modelo/Conexion.java
@@ -20,5 +20,11 @@ public class Conexion {
        Connection con=DriverManager.getConnection(url,"system","system");
        return con;
     }
+    public static Connection conectarse(String login, String password)throws Exception{
+        String url="jdbc:oracle:thin:@localhost:1521:orcl";
+       Class.forName("oracle.jdbc.OracleDriver");
+       Connection con=DriverManager.getConnection(url,login,password);
+       return con;
+    }
     
 }
